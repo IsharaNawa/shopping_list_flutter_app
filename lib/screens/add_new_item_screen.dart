b18ec54898c9bf2d9ef1cdd36300ff258ff3f903
IsharaNawarathna <e17219@eng.pdn.ac.lk>
2024-12-13 01:39:39 +0530

    added initial form

--- lib/screens/add_new_item_screen.dart
+++ lib/screens/add_new_item_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shopping_list_app/data/categories.dart';
 
 class AddNewItemScreen extends StatefulWidget {
   const AddNewItemScreen({super.key});
@@ -14,7 +15,61 @@ class _AddNewItemScreenState extends State<AddNewItemScreen> {
       appBar: AppBar(
         title: const Text("Add new item"),
       ),
-      body: const Text("Form"),
+      body: Form(
+        child: Padding(
+          padding: const EdgeInsets.all(12.0),
+          child: Column(
+            children: [
+              TextFormField(
+                maxLength: 50,
+                decoration: const InputDecoration(
+                  label: Text("Name"),
+                ),
+                validator: (value) => "",
+              ),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  Expanded(
+                    child: TextFormField(
+                      decoration:
+                          const InputDecoration(label: Text("Quantity")),
+                      initialValue: "1",
+                    ),
+                  ),
+                  const SizedBox(
+                    width: 16,
+                  ),
+                  Expanded(
+                      child: DropdownButtonFormField(
+                    items: categories.entries
+                        .map(
+                          (index) => DropdownMenuItem(
+                            value: index.value,
+                            child: Row(
+                              children: [
+                                Container(
+                                  width: 20,
+                                  height: 20,
+                                  color: index.value.color,
+                                ),
+                                const SizedBox(
+                                  width: 10,
+                                ),
+                                Text(index.value.title)
+                              ],
+                            ),
+                          ),
+                        )
+                        .toList(),
+                    onChanged: (value) {},
+                  ))
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
